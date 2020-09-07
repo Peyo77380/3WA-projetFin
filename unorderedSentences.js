fd83640628bf95a77b2f904bd542c1e3ba51e912
Peyo77380 <pierre.guichard77@gmail.com>
2020-09-07 23:43:31 +0200

    Update unorderedSentences.js
    pb sur gestion répétitions bouttons

--- unorderedSentences.js
+++ unorderedSentences.js
@@ -9,7 +9,9 @@ function init () {
 document.addEventListener('DOMContentLoaded', init);
 
 class UnorderedSentencesExam {
+
 	constructor () {
+
 		this.buttons = document.querySelectorAll('.gameButton');
 
 		this.answerFields = document.querySelectorAll('.answer');
@@ -23,8 +25,8 @@ class UnorderedSentencesExam {
 		}
 	}
 
-
 	updateOnKey (el){
+
 		let text = el.target.innerHTML;
 		let relatedContainer = el.target.parentElement.parentElement;
 		let relatedInput = relatedContainer.querySelector('.answer');
@@ -34,11 +36,10 @@ class UnorderedSentencesExam {
 	}
 
 	updateOnClick (el) {
-		console.log(el.target);
+
 		let text = el.target.innerHTML;
 		let relatedContainer = el.target.parentElement.parentElement;
 		let relatedInput = relatedContainer.querySelector('.answer');
-
 
 		if (el.target.classList.contains('selected')) {
 			this.deleteOnClick(el.target, relatedInput, text);
@@ -46,10 +47,8 @@ class UnorderedSentencesExam {
 		} else {
 			this.writeOnClick(el.target, relatedInput, text);
 			this.deleteTrailingSpaces(el.target, relatedInput);
-
 		}
 	}
-
 
 	// au clic : le mot est ajouté dans la réponse
 	writeOnClick (element, relatedInput, text) {
@@ -61,44 +60,81 @@ class UnorderedSentencesExam {
 		relatedInput.value += text;
 
 		element.classList.add('selected');
-
 	}
+
+	// au clic : le mot est supprimé de la réponse
 	deleteOnClick (element, relatedInput, text) {
+
 		let words = relatedInput.value.split(" ");
-		// relatedInput.value = relatedInput.value.replace(text, "");}
 
 		for (let i = 0; i < words.length; i++) {
 			if (words[i] === text) {
 				words.splice(i, 1)
 			}
 		}
-		console.log(words);
 
-		relatedInput.value = words.join(" ");
+		relatedInput.value = words.join(' ');
 		element.classList.remove('selected');
-
-
 	}
-
 
 	// à l'écriture d'un mot dans l'input, le bouton correspondant change de couleur
 	hightlightUsedWord (el) {
 
 		let answer = el.target.value;
-		let wordList = el.target.parentElement.querySelectorAll('.gameButton');
-		let words = answer.split(" ");
+		let buttonsList = el.target.parentElement.querySelectorAll('.gameButton');
 
-		for (let word of wordList) {
-			if (words.includes(word.innerHTML)) {
-				word.classList.add('selected');
-			} else {
-				word.classList.remove('selected');
-			}
+		let answerWords = answer.split(' ');
+		let buttonsWords = [];
 
+		for (let button of buttonsList ) {
+			buttonsWords.push(button.innerHTML);
 		}
 
+
+		let indexedButtonsWords = this.checkForRepetition(buttonsWords);
+
+
+
+
+
+		console.log(indexedButtonsWords);
+
+		// 		word.classList.add('selected');
+		// 	} else {
+		// 		word.classList.remove('selected');
+		// 	}
+		// }
 	}
 
+	checkForRepetition (array) {
+		console.log(array);
+		let rec = [];
+
+		let iteration = {
+					word : array[0],
+					index : [0],
+				}
+		rec.push(iteration);
+
+		// for (let i = 1; i < array.length; i ++) {
+		// 	for (let word of rec){
+		// 		if (array[i] === word.name) {
+		// 			word.index.push(i);
+		// 		} else {
+		// 			iteration = {
+		// 				word : array[i],
+		// 				index : [i],
+		// 			}
+
+		// 			rec.push(iteration);
+		// 		}
+		// 	}
+		// }
+
+		// console.log(rec);
+	}
+
+	// evite les doubles espaces
 	deleteTrailingSpaces (element, relatedInput) {
 
 		let answer = relatedInput.value;
