f581a5035eab619137b9eae6182725bea8416ec0
Peyo77380 <pierre.guichard77@gmail.com>
2020-09-07 14:37:44 +0200

    Unordered Sentence - js in progress
    Unordered Sentence - js in progress

--- unorderedSentences.js
+++ unorderedSentences.js
@@ -1,3 +1,72 @@
 'use strict';
 
-console.log('ok');
+function init () {
+
+	let buttons = document.querySelectorAll('.gameButton');
+
+	for (let button of buttons) {
+		button.addEventListener('click', updateOnClick);
+	}
+
+	let answerFields = document.querySelectorAll('.answer');
+
+	for (let answerField of answerFields) {
+		answerField.addEventListener('keyup', hightlightUsedWord);
+	}
+}
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+function updateOnClick () {
+
+	if (this.classList.contains('selected')) {
+		deleteOnClick(this);
+	} else {
+		writeOnClick(this);
+	}
+}
+
+
+// au clic : le mot est ajouté dans la réponse
+function writeOnClick (element) {
+
+	let text = element.innerHTML;
+	let relatedContainer = element.parentElement.parentElement;
+	let relatedInput = relatedContainer.querySelector('.answer');
+
+	relatedInput.value += text + " ";
+
+	element.classList.add('selected');
+
+}
+function deleteOnClick (element) {
+
+	let text = element.innerHTML;
+	let relatedContainer = element.parentElement.parentElement;
+	let relatedInput = relatedContainer.querySelector('.answer');
+
+	console.log(relatedInput.value.replace(text, ""));
+
+	element.classList.remove('selected');
+
+
+}
+
+
+// à l'écriture d'un mot dans l'input, le bouton correspondant change de couleur
+function hightlightUsedWord () {
+
+	let answer = this.value;
+	let wordList = this.parentElement.querySelectorAll('.gameButton');
+
+	for (let word of wordList) {
+		if (answer.includes(word.innerHTML)) {
+			word.classList.add('selected');
+		} else {
+			word.classList.remove('selected');
+		}
+
+	}
+
+}
