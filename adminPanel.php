4f98d2386ef2685b917e5cbad66863abc5a6e610
Peyo77380 <pierre.guichard77@gmail.com>
2020-09-11 11:39:29 +0200

    début admin
    début admin

---
+++ adminPanel.php
@@ -0,0 +1,8 @@
+
+<aside>
+    <ul>
+        <li><a href="#">Gestion des utilisateurs</a></li>
+        <li><a href="adminExercises.phtml">Gestion des exercices</a></li>
+        <li><a href="#">Gestion des rendez-vous</a></li>
+    </ul>
+</aside>
