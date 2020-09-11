34c724d8a4d4db390b46f1983c297ecc5128a14a
Peyo77380 <pierre.guichard77@gmail.com>
2020-09-11 11:24:23 +0200

    unoreredSentencesOK

--- params/database.php
+++ params/database.php
@@ -19,7 +19,7 @@ class Database
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-            echo "Connected successfully";
+            // echo "Connected successfully";
 
         } catch(PDOException $e) {
 
