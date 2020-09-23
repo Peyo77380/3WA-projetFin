491d78b276eed192a7e5acedd618443e773bac7f
Peyo77380 <pierre.guichard77@gmail.com>
2020-09-23 15:39:01 +0200

    php admin mad libs Manque BDD et js

---
+++ Controllers/adminExercisesMadLibsController.php
@@ -0,0 +1,12 @@
+<?php
+
+$newExercise = new adminExercisesMadLibsController();
+
+
+class adminExercisesMadLibsController
+{
+    public function __construct ()
+    {
+        echo 'coucou';
+    }
+}
