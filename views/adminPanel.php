8156d1bebc3ad5983196deaf76a03cfa01fc391b
Peyo77380 <pierre.guichard77@gmail.com>
2020-10-07 12:08:10 +0200

    menu nettoyé
    affichage exercices en cours.
    bug correction unorderedSentences.phtml
    bug sauvegarde madLibs à corriger

--- views/adminPanel.php
+++
@@ -1,8 +0,0 @@
-
-<aside>
-    <ul>
-        <li><a href="adminUsers.phtml">Gestion des utilisateurs</a></li>
-        <li><a href="adminExercises.phtml">Gestion des exercices</a></li>
-        <li><a href="#">Gestion des rendez-vous</a></li>
-    </ul>
-</aside>
