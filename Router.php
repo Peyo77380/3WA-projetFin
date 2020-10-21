7ef8c8c6c05a2c8bef1b052079f395d851e981c4
Peyo77380 <pierre.guichard77@gmail.com>
2020-10-21 23:36:26 +0200

    test router mvc => controllers OK. faire lien avec vues

--- Router.php
+++ Router.php
@@ -2,25 +2,59 @@
 
 class Router
 {
+    public $request;
+    public $globalRoute;
+    public $research;
+
     public function __construct()
     {
-        $request = $_SERVER['REQUEST_URI'];
 
-        switch ($request) {
-            case '/' :
-                require __DIR__ . '/index.phtml';
-                break;
-            case '' :
-                require __DIR__ . '/index.phtml';
-                break;
-            case '/about' :
-                require __DIR__ . '/about.phtml';
-                break;
-            default:
+        $this->defineRequest();
+
+        $view;
+
+        if ($this->request === '' || $this->request === '/') {
+            require __DIR__ . '/Controllers/Controller.php';
+            require __DIR__ . '/Controllers/indexController.php';
+            $view = new Index();
+
+        } else {
+            try {
+                $controllerName = $this->setTarget();
+
+                require __DIR__ . '/Controllers/Controller.php';
+                require_once($_SERVER['DOCUMENT_ROOT'] . $this->route . 'Controllers/' . $controllerName . '.php');
+
+                $requiredController = new $controllerName ($this->research);
+            } catch (Exception $e) {
                 http_response_code(404);
-                // require __DIR__ . '/views/404.php';
-                break;
+                require __DIR__ . '/Views/404.php';
+
+            }
+
         }
 
+
+    }
+
+    public function defineRequest()
+    {
+        $serverName = '/3WA-projetFin';
+        $this->request = str_replace($serverName, '', $_SERVER['REQUEST_URI']);
+    }
+
+    public function setTarget()
+    {
+
+        $this->research = substr($_SERVER['REQUEST_URI'],
+            strpos($_SERVER['REQUEST_URI'],
+                '/',
+                1),
+            strlen($_SERVER['REQUEST_URI']));
+        $controllerName = ucfirst(trim($this->research, '/')) . 'Controller';
+
+        $this->route = substr_replace($_SERVER['REQUEST_URI'], "", strpos($_SERVER['REQUEST_URI'], $this->research)) . "/";
+
+        return $controllerName;
     }
 }
